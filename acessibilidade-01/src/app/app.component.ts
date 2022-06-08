140fca6cab062ea3bf7bff80bec2fa80f11275b1
dcmunhoz <dc.munhoz@hotmail.com>
2022-06-07 21:28:38 -0300

    Integração com Reactive Forms

--- acessibilidade-01/src/app/app.component.ts
+++ acessibilidade-01/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'acessibilidade-01';
   public yesNoAnswer: string = "no";
+  public test: string = "";
+  public form: FormGroup = new FormGroup({});
+
+
+  constructor(formBuilder: FormBuilder){
+    
+    this.form = formBuilder.group({
+      yesNoAnswer: ['no']
+    });
+
+  }
+
+  public submit(): void{
+    console.log(this.form.value);
+  }
 
 }
--- acessibilidade-01/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ acessibilidade-01/src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,25 +1,47 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
-  styleUrls: ['./yes-no-button-group.component.css']
+  styleUrls: ['./yes-no-button-group.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => YesNoButtonGroupComponent) 
+    }
+  ]
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
 
   @Input() public value: string = "";
   @Input() public label: string = "";
   @Output() public valueChange = new EventEmitter<string>();
   public options = YesNoButtonOption;
+  public onChange = (value: string) => {}
+  public onTouched = (value: string) => {}
 
   constructor() { }
+
+  public writeValue(value: string): void {
+    this.value = value;
+    this.onChange(this.value);
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 
   ngOnInit(): void {
   }
 
   public activate(value: string): void{
-    this.value = value
-    this.valueChange.emit(this.value);
+    this.writeValue(value);
   }
 }
 
